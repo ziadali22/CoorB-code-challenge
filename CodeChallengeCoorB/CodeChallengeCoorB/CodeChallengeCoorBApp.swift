1efed8bf03abde30e3bf9157e59f16eff180f11a
Ziad Khalil <zaskhalil@banquemisr.com>
2025-06-19 22:52:53 +0300

    creating a project

---
+++ CodeChallengeCoorB/CodeChallengeCoorB/CodeChallengeCoorBApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeChallengeCoorBApp.swift
+//  CodeChallengeCoorB
+//
+//  Created by Ziad Khalil on 19/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct CodeChallengeCoorBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CodeChallengeCoorB/CodeChallengeCoorB/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CodeChallengeCoorB
+//
+//  Created by Ziad Khalil on 19/06/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
